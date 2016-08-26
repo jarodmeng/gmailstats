51b55db0623c4a4e118c803d52ca70692152eb28
Jarod G.R. Meng <genrong.meng@gmail.com>
2016-08-26 13:01:33 -0700

    Make Time int64

--- const_type.go
+++ const_type.go
@@ -1,10 +1,6 @@
 package gmailstats
 
-import (
-	"time"
-
-	gmail "google.golang.org/api/gmail/v1"
-)
+import gmail "google.golang.org/api/gmail/v1"
 
 const (
 	defaultClientSecret   = `{"installed":{"client_id":"956558626434-fsr1flbgemibocf40nqotjggalbad08u.apps.googleusercontent.com","project_id":"gmailstats-141502","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"f6lKpgovIdd_RhvVPFDl8ifJ","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}`
@@ -32,10 +28,8 @@ type MessageText struct {
 	BodyText string `json:"bodytext",omitempty`
 }
 
-type JSONTime time.Time
-
 type MessageTime struct {
-	Time JSONTime `json:"time",omitempty`
+	Time int64 `json:"time",omitempty`
 }
 
 type Message struct {
