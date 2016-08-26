c7beea02c7d5109cbecffe849eb843800b39592d
Jarod G.R. Meng <genrong.meng@gmail.com>
2016-08-25 20:01:03 -0700

    Use personal project id

--- const_type.go
+++ const_type.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	defaultClientSecret   = `{"installed":{"client_id":"135069329626-2i4um5rdi49nr98m0bo23dr9hvsbo23a.apps.googleusercontent.com","project_id":"google.com:jarodmeng","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"L8lrG-KCakjXJ0Vs4rZL53TI","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}`
+	defaultClientSecret   = `{"installed":{"client_id":"956558626434-fsr1flbgemibocf40nqotjggalbad08u.apps.googleusercontent.com","project_id":"gmailstats-141502","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"f6lKpgovIdd_RhvVPFDl8ifJ","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}`
 	defaultGmailTokenFile = "gmailstats_token.json"
 	defaultGmailScope     = gmail.GmailReadonlyScope
 	defaultGmailUser      = "me"
